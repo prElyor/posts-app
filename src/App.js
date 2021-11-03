fc7f12a5dd78a39c9aeded5a93b3bba344a67744
Elyor <elyor.sh.99@mail.ru>
2021-11-03 19:53:32 +0300

    #2 Создал нужные страницы, а также навигация по страницам

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import {BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import Posts from './Components/Posts/Posts';
+import PostsEdit from './Components/PostsEdit/PostsEdit';
+import ErrorPage from './Components/Error/ErrorPage';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/posts"/>
+          </Route>
+          <Route exact path="/posts" component={Posts} />
+          <Route exact path="/posts/:id" component={PostsEdit} />
+          <Route component={ErrorPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/PostsEdit/PostsEdit.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PostsEdit() {
+    return (
+        <div>
+            PostEdit
+        </div>
+    )
+}
+
+export default PostsEdit
